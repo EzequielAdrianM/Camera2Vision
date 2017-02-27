cc0c631fa2bbaf428536f4aa6ac46885921f0369
Ezequiel Adrián <ezequieladrianm@gmail.com>
2017-02-27 05:47:57 -0300

    Add files via upload

---
+++ Camera2/app/src/main/java/com/example/ezequiel/camera2/MainActivity.java
@@ -0,0 +1,353 @@
+package com.example.ezequiel.camera2;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.hardware.camera2.CaptureRequest;
+import android.media.Image;
+import android.os.Bundle;
+import android.os.Environment;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.example.ezequiel.camera2.others.Camera2Source;
+import com.google.android.gms.vision.MultiProcessor;
+import com.google.android.gms.vision.Tracker;
+import com.google.android.gms.vision.face.Face;
+import com.google.android.gms.vision.face.FaceDetector;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+
+import com.example.ezequiel.camera2.others.CameraSource;
+import com.example.ezequiel.camera2.others.CameraSourcePreview;
+import com.example.ezequiel.camera2.others.GraphicOverlay;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "Ezequiel Adrian Camera";
+    private Context context;
+    private static final int REQUEST_CAMERA_PERMISSION = 200;
+    private static final int REQUEST_STORAGE_PERMISSION = 201;
+
+    // CAMERA VERSION ONE DECLARATIONS
+    private CameraSource mCameraSource;
+
+    // CAMERA VERSION TWO DECLARATIONS
+    private Camera2Source mCamera2Source;
+
+    // COMMON TO BOTH CAMERAS
+    private CameraSourcePreview mPreview;
+    private FaceDetector previewFaceDetector;
+    private GraphicOverlay mGraphicOverlay;
+    private FaceGraphic mFaceGraphic;
+    private boolean wasActivityResumed = false;
+
+    // DEFAULT CAMERA BEING OPENED
+    private boolean usingFrontCamera = true;
+
+    // YOU CAN CHECK FOR ANDROID API LEVEL 21+ FOR ENABLING CAMERA2 AUTOMATICALLY
+    private boolean useCamera2 = true;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_main);
+        context = getApplicationContext();
+
+        Button takePictureButton = (Button) findViewById(R.id.btn_takepicture);
+        Button switchButton = (Button) findViewById(R.id.btn_switch);
+        mPreview = (CameraSourcePreview) findViewById(R.id.preview);
+        mGraphicOverlay = (GraphicOverlay) findViewById(R.id.faceOverlay);
+
+        requestPermissionThenOpenCamera();
+
+        switchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(usingFrontCamera) {
+                    stopCameraSource();
+                    createCameraSourceBack();
+                    usingFrontCamera = false;
+                } else {
+                    stopCameraSource();
+                    createCameraSourceFront();
+                    usingFrontCamera = true;
+                }
+            }
+        });
+
+        takePictureButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(useCamera2) {
+                    mCamera2Source.takePicture(camera2SourceShutterCallback, camera2SourcePictureCallback);
+                } else {
+                    mCameraSource.takePicture(cameraSourceShutterCallback, cameraSourcePictureCallback);
+                }
+            }
+        });
+    }
+
+    final CameraSource.ShutterCallback cameraSourceShutterCallback = new CameraSource.ShutterCallback() {@Override public void onShutter() {Log.d(TAG, "Shutter Callback!");}};
+    final CameraSource.PictureCallback cameraSourcePictureCallback = new CameraSource.PictureCallback() {
+        @Override
+        public void onPictureTaken(Bitmap picture) {
+            Log.d(TAG, "Taken picture is here!");
+            FileOutputStream out = null;
+            try {
+                out = new FileOutputStream(new File(Environment.getExternalStorageDirectory(), "/camera_picture.png"));
+                picture.compress(Bitmap.CompressFormat.JPEG, 95, out);
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    if (out != null) {
+                        out.close();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    };
+
+    final Camera2Source.ShutterCallback camera2SourceShutterCallback = new Camera2Source.ShutterCallback() {
+        @Override
+        public void onShutter() {Log.d("ASD", "Shutter Callback for CAMERA2");}
+    };
+    final Camera2Source.PictureCallback camera2SourcePictureCallback = new Camera2Source.PictureCallback() {
+        @Override
+        public void onPictureTaken(Image image) {
+            Log.d(TAG, "Taken picture is here!");
+            ByteBuffer buffer = image.getPlanes()[0].getBuffer();
+            byte[] bytes = new byte[buffer.capacity()];
+            buffer.get(bytes);
+            Bitmap picture = BitmapFactory.decodeByteArray(bytes, 0, bytes.length, null);
+            FileOutputStream out = null;
+            try {
+                out = new FileOutputStream(new File(Environment.getExternalStorageDirectory(), "/camera2_picture.png"));
+                picture.compress(Bitmap.CompressFormat.JPEG, 95, out);
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    if (out != null) {
+                        out.close();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    };
+
+    private void requestPermissionThenOpenCamera() {
+        if(ContextCompat.checkSelfPermission(context, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+            if (ContextCompat.checkSelfPermission(context, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+                createCameraSourceFront();
+            } else {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, REQUEST_STORAGE_PERMISSION);
+            }
+        } else {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, REQUEST_CAMERA_PERMISSION);
+        }
+    }
+
+    private void createCameraSourceFront() {
+        previewFaceDetector = new FaceDetector.Builder(context)
+                .setClassificationType(FaceDetector.ALL_CLASSIFICATIONS)
+                .setLandmarkType(FaceDetector.ALL_LANDMARKS)
+                .setMode(FaceDetector.FAST_MODE)
+                .setProminentFaceOnly(true)
+                .setTrackingEnabled(true)
+                .build();
+
+        previewFaceDetector.setProcessor(new MultiProcessor.Builder<>(new GraphicFaceTrackerFactory()).build());
+
+        if(!previewFaceDetector.isOperational()) {
+            Toast.makeText(context, "FACE DETECTION NOT AVAILABLE", Toast.LENGTH_SHORT).show();
+        }
+
+        if(useCamera2) {
+            mCamera2Source = new Camera2Source.Builder(context, previewFaceDetector)
+                    .setFlashMode(CaptureRequest.CONTROL_AE_MODE_ON_ALWAYS_FLASH)
+                    .setFacing(Camera2Source.CAMERA_FACING_FRONT)
+                    .build();
+
+            startCameraSource();
+        } else {
+            mCameraSource = new CameraSource.Builder(context, previewFaceDetector)
+                    .setFacing(CameraSource.CAMERA_FACING_FRONT)
+                    .setRequestedFps(30.0f)
+                    .build();
+
+            startCameraSource();
+        }
+    }
+
+    private void createCameraSourceBack() {
+        previewFaceDetector = new FaceDetector.Builder(context)
+                .setClassificationType(FaceDetector.ALL_CLASSIFICATIONS)
+                .setLandmarkType(FaceDetector.ALL_LANDMARKS)
+                .setMode(FaceDetector.FAST_MODE)
+                .setProminentFaceOnly(true)
+                .setTrackingEnabled(true)
+                .build();
+
+        previewFaceDetector.setProcessor(new MultiProcessor.Builder<>(new GraphicFaceTrackerFactory()).build());
+
+        if(!previewFaceDetector.isOperational()) {
+            Toast.makeText(context, "FACE DETECTION NOT AVAILABLE", Toast.LENGTH_SHORT).show();
+        }
+
+        if(useCamera2) {
+            mCamera2Source = new Camera2Source.Builder(context, previewFaceDetector)
+                    .setFlashMode(CaptureRequest.CONTROL_AE_MODE_ON_ALWAYS_FLASH)
+                    .setFacing(Camera2Source.CAMERA_FACING_BACK)
+                    .build();
+
+            startCameraSource();
+        } else {
+            mCameraSource = new CameraSource.Builder(context, previewFaceDetector)
+                    .setFacing(CameraSource.CAMERA_FACING_BACK)
+                    .setRequestedFps(30.0f)
+                    .build();
+
+            startCameraSource();
+        }
+    }
+
+    private void startCameraSource() {
+        if(useCamera2) {
+            if(mCamera2Source != null) {
+                try {mPreview.start(mCamera2Source, mGraphicOverlay);
+                } catch (IOException e) {
+                    Log.e(TAG, "Unable to start camera source 2.", e);
+                    mCamera2Source.release();
+                    mCamera2Source = null;
+                }
+            }
+        } else {
+            if (mCameraSource != null) {
+                try {mPreview.start(mCameraSource, mGraphicOverlay);
+                } catch (IOException e) {
+                    Log.e(TAG, "Unable to start camera source.", e);
+                    mCameraSource.release();
+                    mCameraSource = null;
+                }
+            }
+        }
+    }
+
+    private void stopCameraSource() {
+        mPreview.stop();
+    }
+
+    private class GraphicFaceTrackerFactory implements MultiProcessor.Factory<Face> {
+        @Override
+        public Tracker<Face> create(Face face) {
+            return new GraphicFaceTracker(mGraphicOverlay);
+        }
+    }
+
+    private class GraphicFaceTracker extends Tracker<Face> {
+        private GraphicOverlay mOverlay;
+
+        GraphicFaceTracker(GraphicOverlay overlay) {
+            mOverlay = overlay;
+            mFaceGraphic = new FaceGraphic(overlay, context);
+        }
+
+        /**
+         * Start tracking the detected face instance within the face overlay.
+         */
+        @Override
+        public void onNewItem(int faceId, Face item) {
+            mFaceGraphic.setId(faceId);
+        }
+
+        /**
+         * Update the position/characteristics of the face within the overlay.
+         */
+        @Override
+        public void onUpdate(FaceDetector.Detections<Face> detectionResults, Face face) {
+            mOverlay.add(mFaceGraphic);
+            mFaceGraphic.updateFace(face);
+        }
+
+        /**
+         * Hide the graphic when the corresponding face was not detected.  This can happen for
+         * intermediate frames temporarily (e.g., if the face was momentarily blocked from
+         * view).
+         */
+        @Override
+        public void onMissing(FaceDetector.Detections<Face> detectionResults) {
+            mFaceGraphic.goneFace();
+            mOverlay.remove(mFaceGraphic);
+        }
+
+        /**
+         * Called when the face is assumed to be gone for good. Remove the graphic annotation from
+         * the overlay.
+         */
+        @Override
+        public void onDone() {
+            mFaceGraphic.goneFace();
+            mOverlay.remove(mFaceGraphic);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == REQUEST_CAMERA_PERMISSION) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                requestPermissionThenOpenCamera();
+            } else {
+                Toast.makeText(MainActivity.this, "CAMERA PERMISSION REQUIRED", Toast.LENGTH_LONG).show();
+                finish();
+            }
+        }
+        if(requestCode == REQUEST_STORAGE_PERMISSION) {
+            if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                requestPermissionThenOpenCamera();
+            } else {
+                Toast.makeText(MainActivity.this, "STORAGE PERMISSION REQUIRED", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if(wasActivityResumed)
+            startCameraSource();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        wasActivityResumed = true;
+        stopCameraSource();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        stopCameraSource();
+        if(previewFaceDetector != null) {
+            previewFaceDetector.release();
+        }
+    }
+}
---
+++ Camera2/app/src/main/java/com/example/ezequiel/camera2/others/CameraSourcePreview.java
@@ -0,0 +1,219 @@
+package com.example.ezequiel.camera2.others;
+
+import android.content.Context;
+import android.graphics.SurfaceTexture;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.Display;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.TextureView;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+
+import com.example.ezequiel.camera2.utils.Utils;
+import com.google.android.gms.common.images.Size;
+
+import java.io.IOException;
+
+import static android.content.Context.WINDOW_SERVICE;
+
+public class CameraSourcePreview extends ViewGroup {
+    private static final String TAG = "CameraSourcePreview";
+
+    //PREVIEW VISUALIZERS FOR BOTH CAMERA1 AND CAMERA2 API.
+    private SurfaceView mSurfaceView;
+    private AutoFitTextureView mAutoFitTextureView;
+
+    private boolean usingCameraOne;
+    private boolean mStartRequested;
+    private boolean mSurfaceAvailable;
+    private boolean viewAdded = false;
+
+    //CAMERA SOURCES FOR BOTH CAMERA1 AND CAMERA2 API.
+    private CameraSource mCameraSource;
+    private Camera2Source mCamera2Source;
+
+    private GraphicOverlay mOverlay;
+    private int screenWidth;
+    private int screenHeight;
+    private int screenRotation;
+
+    public CameraSourcePreview(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        screenHeight = Utils.getScreenHeight(context);
+        screenWidth = Utils.getScreenWidth(context);
+        screenRotation = Utils.getScreenRotation(context);
+        mStartRequested = false;
+        mSurfaceAvailable = false;
+        mSurfaceView = new SurfaceView(context);
+        mSurfaceView.getHolder().addCallback(mSurfaceViewListener);
+        mAutoFitTextureView = new AutoFitTextureView(context);
+        mAutoFitTextureView.setSurfaceTextureListener(mSurfaceTextureListener);
+    }
+
+    public void start(CameraSource cameraSource, GraphicOverlay overlay) throws IOException {
+        usingCameraOne = true;
+        mOverlay = overlay;
+        start(cameraSource);
+    }
+
+    public void start(Camera2Source camera2Source, GraphicOverlay overlay) throws IOException {
+        usingCameraOne = false;
+        mOverlay = overlay;
+        start(camera2Source);
+    }
+
+    private void start(CameraSource cameraSource) throws IOException {
+        if (cameraSource == null) {stop();}
+        mCameraSource = cameraSource;
+        if (mCameraSource != null) {
+            mStartRequested = true;
+            if(!viewAdded) {
+                addView(mSurfaceView);
+                viewAdded = true;
+            }
+            try {startIfReady();} catch (IOException e) {Log.e(TAG, "Could not start camera source.", e);}
+        }
+    }
+
+    private void start(Camera2Source camera2Source) throws IOException {
+        if (camera2Source == null) {stop();}
+        mCamera2Source = camera2Source;
+        if(mCamera2Source != null) {
+            mStartRequested = true;
+            if(!viewAdded) {
+                addView(mAutoFitTextureView);
+                viewAdded = true;
+            }
+            try {startIfReady();} catch (IOException e) {Log.e(TAG, "Could not start camera source.", e);}
+        }
+    }
+
+    public void stop() {
+        mStartRequested = false;
+        if(usingCameraOne) {
+            if (mCameraSource != null) {
+                mCameraSource.stop();
+            }
+        } else {
+            if(mCamera2Source != null) {
+                mCamera2Source.stop();
+            }
+        }
+    }
+
+    private void startIfReady() throws IOException {
+        if (mStartRequested && mSurfaceAvailable) {
+            try {
+                if(usingCameraOne) {
+                    mCameraSource.start(mSurfaceView.getHolder());
+                    if (mOverlay != null) {
+                        Size size = mCameraSource.getPreviewSize();
+                        if(size != null) {
+                            int min = Math.min(size.getWidth(), size.getHeight());
+                            int max = Math.max(size.getWidth(), size.getHeight());
+                            // FOR GRAPHIC OVERLAY, THE PREVIEW SIZE WAS REDUCED TO QUARTER
+                            // IN ORDER TO PREVENT CPU OVERLOAD
+                            mOverlay.setCameraInfo(min/4, max/4, mCameraSource.getCameraFacing());
+                            mOverlay.clear();
+                        } else {
+                            stop();
+                        }
+                    }
+                    mStartRequested = false;
+                } else {
+                    mCamera2Source.start(mAutoFitTextureView, screenRotation);
+                    if (mOverlay != null) {
+                        Size size = mCamera2Source.getPreviewSize();
+                        if(size != null) {
+                            int min = Math.min(size.getWidth(), size.getHeight());
+                            int max = Math.max(size.getWidth(), size.getHeight());
+                            // FOR GRAPHIC OVERLAY, THE PREVIEW SIZE WAS REDUCED TO QUARTER
+                            // IN ORDER TO PREVENT CPU OVERLOAD
+                            mOverlay.setCameraInfo(min/4, max/4, mCamera2Source.getCameraFacing());
+                            mOverlay.clear();
+                        } else {
+                            stop();
+                        }
+                    }
+                    mStartRequested = false;
+                }
+            } catch (SecurityException e) {Log.d(TAG, "SECURITY EXCEPTION: "+e);}
+        }
+    }
+
+    private final SurfaceHolder.Callback mSurfaceViewListener = new SurfaceHolder.Callback() {
+        @Override
+        public void surfaceCreated(SurfaceHolder surface) {
+            mSurfaceAvailable = true;
+            mOverlay.bringToFront();
+            try {startIfReady();} catch (IOException e) {Log.e(TAG, "Could not start camera source.", e);}
+        }
+        @Override
+        public void surfaceDestroyed(SurfaceHolder surface) {
+            mSurfaceAvailable = false;
+        }
+        @Override
+        public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {}
+    };
+
+    private final TextureView.SurfaceTextureListener mSurfaceTextureListener = new TextureView.SurfaceTextureListener() {
+        @Override
+        public void onSurfaceTextureAvailable(SurfaceTexture texture, int width, int height) {
+            mSurfaceAvailable = true;
+            mOverlay.bringToFront();
+            try {startIfReady();} catch (IOException e) {Log.e(TAG, "Could not start camera source.", e);}
+        }
+        @Override
+        public void onSurfaceTextureSizeChanged(SurfaceTexture texture, int width, int height) {}
+        @Override
+        public boolean onSurfaceTextureDestroyed(SurfaceTexture texture) {
+            mSurfaceAvailable = false;
+            return true;
+        }
+        @Override
+        public void onSurfaceTextureUpdated(SurfaceTexture texture) {}
+    };
+
+    @Override
+    protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
+        int width = 480;
+        int height = 720;
+        if(usingCameraOne) {
+            if (mCameraSource != null) {
+                Size size = mCameraSource.getPreviewSize();
+                if (size != null) {
+                    // Swap width and height sizes when in portrait, since it will be rotated 90 degrees
+                    height = size.getWidth();
+                    width = size.getHeight();
+                }
+            }
+        } else {
+            if (mCamera2Source != null) {
+                Size size = mCamera2Source.getPreviewSize();
+                if (size != null) {
+                    // Swap width and height sizes when in portrait, since it will be rotated 90 degrees
+                    height = size.getWidth();
+                    width = size.getHeight();
+                }
+            }
+        }
+
+        //RESIZE PREVIEW IGNORING ASPECT RATIO. THIS IS ESSENTIAL.
+        int newWidth = (height * screenWidth) / screenHeight;
+
+        final int layoutWidth = right - left;
+        final int layoutHeight = bottom - top;
+        // Computes height and width for potentially doing fit width.
+        int childWidth = layoutWidth;
+        int childHeight = (int)(((float) layoutWidth / (float) newWidth) * height);
+        // If height is too tall using fit width, does fit height instead.
+        if (childHeight > layoutHeight) {
+            childHeight = layoutHeight;
+            childWidth = (int)(((float) layoutHeight / (float) height) * newWidth);
+        }
+        for (int i = 0; i < getChildCount(); ++i) {getChildAt(i).layout(0, 0, childWidth, childHeight);}
+        try {startIfReady();} catch (IOException e) {Log.e(TAG, "Could not start camera source.", e);}
+    }
+}
